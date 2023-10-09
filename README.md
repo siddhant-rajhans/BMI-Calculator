53b08346b3b44509c5684aaa6b32729d72ededf9
Siddhant Rajhans <51508945+siddhant-rajhans@users.noreply.github.com>
2023-10-09 14:20:24 +0530

    created BMI app

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
-# bmi_calculator
+# BMI Calculator App
 
-A new Flutter project.
+[![GitHub stars](https://img.shields.io/github/stars/yourusername/BMI-Calculator-App.svg)](https://github.com/yourusername/BMI-Calculator-App/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/yourusername/BMI-Calculator-App.svg)](https://github.com/yourusername/BMI-Calculator-App/network)
+[![GitHub issues](https://img.shields.io/github/issues/yourusername/BMI-Calculator-App.svg)](https://github.com/yourusername/BMI-Calculator-App/issues)
+
+## Description
+
+The BMI (Body Mass Index) Calculator is a powerful tool designed to help users monitor their health and fitness journey. This user-friendly app provides a quick and easy way to calculate BMI and gain valuable insights into overall health.
+
+![App Screenshot](assets/images/app_ss.png)
+
+## Features
+
+- **Calculate BMI:** Enter height and weight to instantly calculate BMI.
+- **BMI Interpretation:** Get clear BMI category interpretations.
+- **Personalized Recommendations:** Receive health recommendations based on BMI.
+- **History Tracking:** Record BMI calculations over time.
+- **User-Friendly Interface:** Easy navigation for users of all levels.
+- **Metric and Imperial Units:** Support for height and weight units.
+- **Share Your Progress:** Share results and progress with others.
+
+## Why Use the BMI Calculator App
+
+Maintaining a healthy BMI is essential for overall well-being. Whether you're on a weight loss journey, working to maintain your current weight, or simply curious about your health, the BMI Calculator app is your go-to tool. It provides valuable insights into body composition and offers personalized recommendations to help achieve health and fitness goals.
+
+Stay informed, stay healthy, and take control of your fitness journey with the BMI Calculator app. Download it today and embark on the path to a healthier you!
+
+## Contribution Guidelines
+
+We welcome contributions from the developer community to enhance the BMI Calculator app. To contribute:
+
+1. Fork this repository.
+2. Create a new branch for your feature or bug fix: `git checkout -b feature-name`
+3. Make your changes and commit them: `git commit -m 'Description of changes'`
+4. Push to the branch: `git push origin feature-name`
+5. Submit a pull request.
+
+We appreciate your contributions and look forward to collaborating with you!
+
+## Getting Started
+
+To get started with the BMI Calculator app:
+
+1. Clone this repository: `git clone https://github.com/yourusername/BMI-Calculator-App.git`
+2. Open the project in your preferred Flutter development environment.
+3. Run the app on an emulator or physical device.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+**Note:** Replace `yourusername` and `app-screenshot.png` with your actual GitHub username and a screenshot of your app.
+
+For more information on how to customize this README.md file, refer to the [GitHub Markdown Guide](https://guides.github.com/features/mastering-markdown/).
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp( MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  var Weight_Controller = TextEditingController();
+
+  var FtHeight_Controller = TextEditingController();
+
+  var InHeight_Controller = TextEditingController();
+
+  var Result = "";
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
 
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title:const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FaIcon(Icons.monitor_weight, size: 42,), // Centered logo
+              SizedBox(width: 8), // Add spacing between logo and title
+              Text('BMI Calculator', style: TextStyle(
+                fontSize: 24, // Increased font size for title text
+                fontWeight: FontWeight.w900
+              )), // Centered title text
+            ],
+          ),
+        ),
+
+
+
+        body: Center(
+          child: Container(
+            // color: Colors.black,
+            width: 300,
+            height: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //If you want to add an image
+              //   const Image(image: AssetImage('assets/images/img.png'),
+              //       fit: BoxFit.fill),
+              //
+              //   const SizedBox(height: 40,),
+              //   const Text('Enter details:', style: TextStyle(color: Colors.white)),
+              // const SizedBox(height: 40,),
+
+
+                TextField(
+                  controller: Weight_Controller,
+                  decoration: const InputDecoration(
+                      fillColor: Colors.white,filled: true,
+                      prefixIcon: Icon(Icons.line_weight, size: 45,),
+                      label: Text('Enter your weight(in kg)') ),
+                  keyboardType: TextInputType.number,
+
+
+                ),
+
+                const SizedBox(height: 30,),
+
+                TextField(
+                  controller: FtHeight_Controller,
+                  decoration: const InputDecoration(
+                      fillColor: Colors.white,filled: true,
+                      prefixIcon: Icon(Icons.height, size: 45,),
+                      label: Text('Enter your height (in Ft)') ),
+                  keyboardType: TextInputType.number,
+                ),
+
+                const SizedBox(height: 30),
+
+                TextField(
+                  controller: InHeight_Controller,
+                  decoration: const InputDecoration(
+                      fillColor: Colors.white,filled: true,
+                      prefixIcon: Icon(Icons.height, size: 45,),
+                      label: Text('Enter your height (Inch)') ),
+                  keyboardType: TextInputType.number,
+                ),
+
+            const SizedBox(height: 30,),
+
+                ElevatedButton(onPressed: (){
+
+                  var weight = Weight_Controller.text.toString();
+                  var feet = FtHeight_Controller.text.toString();
+                  var inch = InHeight_Controller.text.toString();
+
+                  if(weight!= "" && feet != ""){
+                  //BMI Calculation
+                    var int_Weight =int.parse(weight);
+                    var int_inch = int.parse(inch);
+                    var int_feet = int.parse(feet);
+
+                    var total_inches = (int_feet * 12) + int_inch ;
+                    var total_cetimeter = total_inches * 2.54 ;
+                    var total_meter = total_cetimeter/100 ;
+
+                    var BMI = int_Weight/(total_meter * total_meter);
+
+                    setState(() {
+                      Result = "Your BMI is: ${BMI.toStringAsFixed(2)}";
+                    });
+                  }
+                  else{
+                      setState((){
+                        Result = "Please fill all the required blanks";
+                      });
+                  }
+
+                },
+                    child: Text("Calculate")),
+                SizedBox(height: 30,),
+                Text(Result, style: TextStyle(color: Colors.white),)
+              ],
+            ),
+          ),
         ),
       ),
     );
